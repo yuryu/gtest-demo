cf36aae4ff0b5007789324dde6bffaaaafbb1a41
Haruka Iwao <yuryu@yuryu.jp>
2015-02-12 15:41:11 +0900

    add tests for sub()

--- test/func_test.cc
+++ test/func_test.cc
@@ -11,3 +11,15 @@ TEST(test_add, handles_positive) {
     EXPECT_EQ(7, add(3, 4));
 }
 
+TEST(test_sub, handles_zero) {
+    EXPECT_EQ(0, sub(0, 0));
+}
+
+TEST(test_sub, handles_positive) {
+    EXPECT_EQ(2, sub(5, 3));
+}
+
+TEST(test_sub, handles_negative) {
+    EXPECT_EQ(-1, sub(1, 2));
+}
+
